4f095fb9e3dc556a618a444f826a9eb53ad34972
Ivan Ermilov <earthquakesan@gmail.com>
2016-11-21 11:10:31 +0100

    revert run.py for generation of tables

--- run.py
+++ run.py
@@ -1,17 +1,17 @@
-# from opentablebench.DataGeneratorRunner import DataGeneratorRunner
+from opentablebench.DataGeneratorRunner import DataGeneratorRunner
+
+RUNNER = DataGeneratorRunner()
+RUNNER.run()
+
+#from opentablebench.NLTKInterface import cluster_header_random
+#from opentablebench.NLTKInterface import load_test_data
+#from opentablebench.NLTKInterface import \
+#    calculate_maximum_complexity_combinations, \
+#    calculate_maximum_complexity_naive
 #
-# RUNNER = DataGeneratorRunner()
-# RUNNER.run()
-
-from opentablebench.NLTKInterface import cluster_header_random
-from opentablebench.NLTKInterface import load_test_data
-from opentablebench.NLTKInterface import \
-    calculate_maximum_complexity_combinations, \
-    calculate_maximum_complexity_naive
-
-# print calculate_maximum_complexity_naive()
-# print calculate_maximum_complexity_combinations()
-
-headers = load_test_data()
-print headers[0]
-print cluster_header_random(headers[0])
+## print calculate_maximum_complexity_naive()
+## print calculate_maximum_complexity_combinations()
+#
+#headers = load_test_data()
+#print headers[0]
+#print cluster_header_random(headers[0])
